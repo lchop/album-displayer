ccf1f9ad09719093d91dae97e05e5c7deec04b9b
Louis CHOPOT <louis.chopot@atoptima.com>
2022-07-01 14:05:46 +0200

    improuve

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -13,8 +13,10 @@ export class MainPageComponent implements OnInit {
   public slides = [
     { src: "/assets/images/greenMag.jpeg" },
     { src: "/assets/images/guyaMag.png" },
-    { src: "/assets/images/greenMag.jpeg" },
-    { src: "/assets/images/guyaMag.png" }
+    { src: "/assets/images/Elle.png" },
+    { src: "/assets/images/ewag.webp" },
+    { src: "/assets/images/resto-avenue.png" },
+    { src: "/assets/images/shopping-fr.png" },
   ];
 
   constructor(private articleService: ArticleService) {}
