3c3fe03fc9a651e05b10463b4030b1fffbc414ff
Louis CHOPOT <louis.chopot@atoptima.com>
2022-07-13 15:39:06 +0200

    regex+handle wrong type

--- src/app/page/contact-page/contact-page.component.ts
+++ src/app/page/contact-page/contact-page.component.ts
@@ -7,25 +7,87 @@ import { ContactService } from './contact.service';
   templateUrl: './contact-page.component.html'
 })
 export class ContactPageComponent implements OnInit {
-  company: string;
-  email: string;
-  name: string;
-  message: string;
+  company: string = '';
+  email: string = '';
+  name: string = '';
+  message: string = '';
   waitResponse: boolean = false;
+  isDisabled = true;
+
+  correctMail = true;
+  correctName = true;
+  correctCompany = true;
+  correctMessage = true;
 
   constructor(private contact: ContactService) { }
 
   ngOnInit(): void {
   }
 
-  onSubmit(form: NgForm) { 
-    this.waitResponse = true;
-    this.contact.postMessageContact(form.value)
-      .subscribe(response => {
-        location.href = 'https://mailthis.to/confirm'
-      this.waitResponse = false;
-      }, error => {
-      console.log({ error })
-      })
+  onKeyUp(fromInput: string) {
+    this.checkInput(fromInput);
+  }
+
+  checkInput(fromInput: string) : boolean{
+      switch (fromInput) {
+        case 'name':
+          const name = /^[a-zA-Z_ -]{2,}$/.test(this.name)
+          if(name){
+            this.correctName = true;
+          } else{
+            this.correctName = false;
+          }
+          break;
+        case 'email':
+          const email = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(this.email)
+          if (email) {
+            this.correctMail = true;
+          } else {
+            this.correctMail = false;
+          }
+          break;
+        case 'company':
+          const company = /^[\w_ -]{2,}$/.test(this.company)
+          if (company) {
+            this.correctCompany = true;
+          } else {
+            this.correctCompany = false;
+          }
+          break;
+        case 'message':
+          const message =  this.message.length >= 2? true : false;
+          if (message) {
+            this.correctMessage = true;
+          } else {
+            this.correctMessage = false;
+          }
+          break;
+        default:
+          break;
+      }
+      if (this.correctMessage && this.correctCompany && this.correctName && this.correctMail) {
+        this.isDisabled = false;
+        return true;
+      }
+      else
+      {
+        this.isDisabled = true;
+        return false;
+      }
+    }
+
+  onSubmit(form: NgForm) {
+    if (this.checkInput('email') && this.checkInput('name') 
+    && this.checkInput('company') && this.checkInput('message')) {      
+      this.waitResponse = true;
+      form.value.company.test;
+      this.contact.postMessageContact(form.value)
+        .subscribe(response => {
+          location.href = 'https://mailthis.to/confirm'
+        this.waitResponse = false;
+        }, error => {
+        console.log({ error })
+        })
+    }
   }
 }
