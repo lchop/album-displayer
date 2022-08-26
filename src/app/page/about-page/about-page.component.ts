148e706351e44268aeec0e3bc1cb5dbf66fa22b8
Louis CHOPOT <louis.chopot@atoptima.com>
2022-08-26 11:47:35 +0200

    remove client

--- src/app/page/about-page/about-page.component.ts
+++ src/app/page/about-page/about-page.component.ts
@@ -9,7 +9,6 @@ export class AboutPageComponent implements OnInit {
   public slides = [
     { src: "/assets/images/greenMag.jpeg" },
     { src: "/assets/images/guyaMag.png" },
-    { src: "/assets/images/Elle.png" },
     { src: "/assets/images/ewag.webp" },
     { src: "/assets/images/shopping-fr.png" },
     { src: "/assets/images/resto-avenue.png" }
